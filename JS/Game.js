2ca19f6aa6a70e155a465b39872a2bda42f35bfe
unknown <mleong@skidmore.edu>
2020-10-11 18:18:56 -0400

    Refactored game logic into game logic class, cleaned up the Intro.JS file.

--- JS/Game.js
+++ JS/Game.js
@@ -1,11 +1,57 @@
 class Game{
-    constructor(player, platArray, controller, canvas, ctx){
+    constructor(player, platArray, controller, gameWorld){
         this.player = player;
         this.platArray = platArray;
         this.controller = controller;
-        this.canvas = canvas;
-        this.ctx = ctx;
+        this.gameWorld = gameWorld;
+    }
+
+    updateAllPlats(){
+        this.platArray.forEach(function(plat){
+            plat.update(gameworld);
+        })
+    }
+
+    updatePlayer(){
+        this.player.update(this.gameWorld);
+    }
+
+    controlPlayer(){
+        //if checks can't be conditional here otherwise you won't be able to run and jump
+        //https://stackoverflow.com/questions/5203407/how-to-detect-if-multiple-keys-are-pressed-at-once-using-javascript
+        //game.checkControllerRightPressed();
+        if(this.controller.rightPressed){
+            this.player.xVelocity += 5;
+        }
+        if(this.controller.leftPressed){
+            this.player.xVelocity -= 5;
+        }
+        if(this.controller.upPressed){
+            this.player.jump(canvas.height * 0.1);
+        }
     }
 
 
+    stopPlayerOutOfBoundsResize() {
+        if (this.controller.leftPressed == false && this.controller.rightPressed == false && this.controller.upPressed == false) {
+            //buffer of 3
+            this.player.prevCollisionPointY = this.player.collisionPointY - 40;
+        }
+    }
+
+    keepPlayerInXBounds(){
+        if(this.player.x < -this.player.w){
+            this.player.x = this.gameWorld.currWidth;
+        } else if (this.player.x > canvas.width){
+            this.player.x = -this.player.w
+        }
+    }
+
+    checkCollisions(){
+        //https://stackoverflow.com/questions/29626729/how-to-function-call-using-this-inside-foreach-loop/29626762
+        let self = this;
+        this.platArray.forEach(function(plat){
+            self.player.checkCollision(plat);
+        })
+    }
 }
--- JS/Intro.js
+++ JS/Intro.js
@@ -7,9 +7,9 @@ let oldWidth = canvas.width;
 let oldHeight = canvas.height;
 /*outOfBounds here is just a little buffer that I end up adding to the floor to give 100 pixels to the left and 100 to the
   right more space.*/
-let outOfBounds = canvas.width/2;
-
 var gameworld = new GameWorld(canvas, ctx, oldWidth, oldHeight);
+//this line will probably work if we remove all the canvas calls when creating gameobjects
+//gameworld.fitToParentContainer();
 
 //todo develop a ratio so that pixel art isn't scrunched
 //todo look into import statements instead of loading each separate JS file
@@ -75,8 +75,6 @@ jumpLeftImg.src = 'Assets/pixelMeFolderLeft/pixelMeJumpLeft.png';
 var picLeftArray = [idleImgLeft, moveLeftImg1, moveLeftImg2, moveLeftImg3, moveLeftImg4, moveLeftImg5,
     moveLeftImg6, moveLeftImg7];
 
-
-
 function fitToParentContainer(canvas) {
     canvas.style.width = '100%';
     canvas.style.height = '100%';
@@ -84,127 +82,80 @@ function fitToParentContainer(canvas) {
     canvas.height = canvas.offsetHeight;
 }
 
-function updateOldCanvas(){
-    if(canvas.width != oldWidth){
-        oldWidth = canvas.width;
+var controller = new Controller();
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+function keyDownHandler(e){
+    if (e.key == 'd') {
+        //move right
+        controller.rightPressed = true;
+        console.log(this.rightPressed);
     }
-
-    if(canvas.height != oldHeight){
-        oldHeight = canvas.height;
+    if (e.key == 'a'){
+        //move left
+        controller.leftPressed = true;
+    }
+    if(e.key == "w"){
+        //jump
+        controller.upPressed = true;
     }
 }
 
-function logInformation(){
-    console.log("This is the canvas height: " + canvas.height);
-    //console.log("This is player's x: " + matt.x);
-    //console.log("This is player's y: " + matt.y);
-    console.log("This is the player's feet: " + matt.collisionPointY);
-    //console.log("This is collision point x: " + matt.collisionPointX);
-    console.log("This is the platform's y: " + theFloor.y);
-    //console.log("Canvas height - matt.h: " + matt.h);
-    //console.log("This is matt's yVe;: " + matt.yVel);
+function keyUpHandler(e){
+    if (e.key == 'd') {
+        //move right
+        console.log("right key is not pressed.");
+        controller.rightPressed = false;
+        console.log(this.rightPressed);
+    }
+    if(e.key == 'a'){
+        //move left
+        controller.leftPressed = false;
+    }
+    if(e.key == 'w'){
+        //jump
+        controller.upPressed = false;
+    }
 }
 
 //There should be a start game function to put everything in their starting places
 //There should be a reset to be called when the canvas isn't in view
 //Classes player, platforms (extend platforms so that they can have textures and not just words), items, gameObj, and controller
 
-//*****Classes*****//
 
-
-
-
-
-/*
-Controller inputs, need to think of a better way to handle these
- */
-
-var rightPressed = false
-var leftPressed = false;
-var upPressed = false;
-
-
-var controller = new Controller();
-document.addEventListener('keydown', controller.keyDownHandler);
-document.addEventListener('keyup', controller.keyUpHandler);
-
-
-var theFloor = new Platform(0 - outOfBounds, canvas.height - 2, canvas.width + (outOfBounds * 2), 2);
-var matt = new Player(canvas.width - canvas.width * .95, canvas.height * 0.65,
-    canvas.width * .18, canvas.height *.3, picArray, picLeftArray);
+var theFloor = new Platform(0 - gameworld.outOfBounds, gameworld.currHeight - 2, gameworld.currWidth + (gameworld.outOfBounds * 2), 2);
+var matt = new Player(gameworld.currWidth - gameworld.currWidth * .95, gameworld.currHeight * 0.65,
+    gameworld.currWidth * .18, gameworld.currHeight *.3, picArray, picLeftArray);
 var textPlatTest2 = new TextPlatform(0, Math.round(canvas.height * 0.50), canvas.width/2, 40, "With an interest in front end design");
 var textPlatTest1 = new TextPlatform(canvas.width/2 - canvas.width * 0.11, Math.round(canvas.height * 0.80), canvas.width * 0.22,
     40, "A programmer");
 var textPlatTest3 = new TextPlatform(canvas.width/2 - canvas.width * 0.11, Math.round(canvas.height * 0.20), canvas.width * 0.22,
     40, "Matthew Leong");
 var platArray = [textPlatTest2, theFloor, textPlatTest1, textPlatTest3];
-
-//document.addEventListener('resize', matt.resizePrevColPoint);
+var game = new Game(matt, platArray, controller, gameworld);
 
 function animate(){
-    requestAnimationFrame(animate);
+
     //fit to parent container needs to be here to resize the canvas whenever the screen is resized
     //fit to parent also seems to control redrawing the canvas
-    gameworld.fitToParentContainer();
+    game.gameWorld.fitToParentContainer();
 
-    platArray.forEach(function(plat){
-        plat.update(gameworld);
-    })
+    game.updateAllPlats();
 
-    /*
-    console.log("This is the floors top after the plat update: " + textPlatTest1.top);
-    console.log("This is the players previous y Col point after the plat update: " + matt.prevCollisionPointY);
-    */
+    game.controlPlayer();
 
-    //console.log("Inside the game loop: " + controller.rightPressed + " " + matt.xVel + " ");
-    //if checks can't be conditional here otherwise you won't be able to run and jump
-    //https://stackoverflow.com/questions/5203407/how-to-detect-if-multiple-keys-are-pressed-at-once-using-javascript
-    if(rightPressed){
-        console.log("Got here");
-        matt.xVelocity += 5;
-    }
-    if(leftPressed){
-        matt.xVelocity -= 5;
-    }
-    if(upPressed){
-        matt.jump(canvas.height * 0.1);
-    }
-
-    matt.update(gameworld);
+    game.updatePlayer();
 
     //this is the right spot for this for sure, no clue why it has to be - 40 as opposed to 3 or 10
-    if(leftPressed == false && rightPressed == false && upPressed == false){
-        console.log("Im here");
-        //buffer of 3
-        matt.prevCollisionPointY = matt.collisionPointY - 40;
-    } else {
-        console.log("First few frames");
-    }
+    game.stopPlayerOutOfBoundsResize();
 
-    /*
-    console.log("This is the floors top after the matt update: " + textPlatTest1.top);
-    console.log("This is the players previous y Col point after the matt update: " + matt.prevCollisionPointY);
-     */
-    //this is probably not where this loop should go, probably in the board class down the line
-    //controls scrolling so you don't go out of bounds of the canvas
-    if(matt.x < -matt.w){
-        matt.x = canvas.width;
-    } else if (matt.x > canvas.width){
-        matt.x = -matt.w;
-    }
+    game.keepPlayerInXBounds();
 
+    game.checkCollisions();
 
+    game.gameWorld.updateOldCanvas();
 
-
-    platArray.forEach(function (plat) {
-        matt.checkCollision(plat);
-    })
-
-
-
-
-    gameworld.updateOldCanvas();
-    //logInformation();
+    requestAnimationFrame(animate);
 }
 
 animate();
